a50d42dc20ce7206acd578e8d7ce45c0ee8135ef
Will Roscoe <wroscoe@gmail.com>
2018-12-02 13:33:55 -0800

    increment version to 0.1.1

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(name='donkeypart_tub',
-      version='0.1',
+      version='0.1.1',
       description='JSON based datastore for the donkeycar.',
       long_description="no long description given",
       long_description_content_type="text/markdown",
